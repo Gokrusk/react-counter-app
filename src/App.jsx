e776f2fd21eb23bbd2617da878cb28e564a1fb57
gokrusk <nigelljama@gmail.com>
2023-02-14 21:08:54 -0500

    rendering buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import Button from './components/button';
 import espoch from './images/logoico.png';
 
 function App() {
+  const add = () => {
+    console.log('clic')
+  }
+  const reset = () => {
+    console.log('reset')
+  }
   return (
     <div className='App'>
       <div className='espoch-logo-contenedor'>
@@ -8,6 +15,16 @@ function App() {
         <img className='espochLogo' src={espoch} alt='Logo ESPOCH' />
       </div>
       <div className='contenedor'></div>
+      <Button
+        text = 'ADD'
+        buttonClick={true}
+        manejarClic={add}
+      />
+      <Button
+        text = 'RESET'
+        buttonClick={false}
+        manejarClic={reset}
+      />
     </div>
   );
 }
